8c689fec886f10997b43b9e3d358f21d01858787
David Liencura <davidw.liencura@gmail.com>
2023-04-08 20:44:24 -0400

    fix telescope

--- nvim/lua/david/plugins-setup.lua
+++ nvim/lua/david/plugins-setup.lua
@@ -29,6 +29,7 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
   -- fuzzy finding w/ telescope
+  use 'nvim-lua/plenary.nvim'
   use{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" } -- dependency for better sorting performance
   use{ "nvim-telescope/telescope.nvim", branch = "0.1.x" } -- fuzzy finder
 
